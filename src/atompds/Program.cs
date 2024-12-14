9c5a0879e621613838fe17969f91a567540c2fd9
Passive <20432486+PassiveModding@users.noreply.github.com>
2024-12-14 22:45:59 +1100

    Reformat weird indentation

--- src/atompds/Program.cs
+++ src/atompds/Program.cs
@@ -4,7 +4,6 @@ using atompds.Config;
 using atompds.Middleware;
 using Microsoft.AspNetCore.HttpLogging;
 using Microsoft.EntityFrameworkCore;
-using Sequencer;
 using Sequencer.Db;
 
 namespace atompds;
@@ -14,53 +13,53 @@ public class Program
     public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateSlimBuilder(args);
-        
+
         builder.Services.AddCors();
         builder.Services.AddHttpClient();
-        
+
         // validate server environment
         var environment = builder.Configuration.GetSection("Config").Get<ServerEnvironment>() ?? throw new Exception("Missing server environment configuration");
         var serverConfig = new ServerConfig(environment);
-        
+
         ServerConfig.RegisterServices(builder.Services, serverConfig);
-        
+
         // response serialize, ignore when writing default
         builder.Services.AddControllers().AddJsonOptions(options =>
-		{
-	        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
-		});
+        {
+            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+        });
 
         builder.Services.AddHttpLogging(logging =>
         {
-	        logging.LoggingFields = HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode |
-	                                HttpLoggingFields.RequestMethod;
-			logging.CombineLogs = true;
-		});
-        
+            logging.LoggingFields = HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode |
+                                    HttpLoggingFields.RequestMethod;
+            logging.CombineLogs = true;
+        });
+
 
         var app = builder.Build();
-        
+
         using (var scope = app.Services.CreateScope())
-		{
-			var accountManager = scope.ServiceProvider.GetRequiredService<AccountManagerDb>();
-			await accountManager.Database.MigrateAsync();
-			
-			var seqDb = scope.ServiceProvider.GetRequiredService<SequencerDb>();
-			await seqDb.Database.MigrateAsync();
-		}
+        {
+            var accountManager = scope.ServiceProvider.GetRequiredService<AccountManagerDb>();
+            await accountManager.Database.MigrateAsync();
+
+            var seqDb = scope.ServiceProvider.GetRequiredService<SequencerDb>();
+            await seqDb.Database.MigrateAsync();
+        }
 
         app.UseRouting();
         app.MapControllers();
-        app.UseExceptionHandler("/error");	
+        app.UseExceptionHandler("/error");
         app.UseAuthMiddleware();
         app.UseNotFoundMiddleware();
         app.UseWebSockets();
-        
+
         app.UseCors(cors => cors.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-        
+
         if (app.Environment.IsDevelopment())
         {
-	        //app.UseHttpLogging();
+            //app.UseHttpLogging();
         }
 
         var logger = app.Services.GetRequiredService<ILogger<Program>>();
