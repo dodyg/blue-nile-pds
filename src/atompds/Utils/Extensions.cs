63aa25e700c59a5bb01655bcca682d3096ddd540
Dody Gunawinata <dodyg@silverkeytech.com>
2025-08-17 13:19:04 +0300

    Use C# 14

--- src/atompds/Utils/Extensions.cs
+++ src/atompds/Utils/Extensions.cs
@@ -7,23 +7,32 @@ namespace atompds.Utils;
 
 public static class Extensions
 {
-    public static DidDoc ToDidDoc(this DidDocument document)
+    extension(DidDocument document)
     {
-        // TODO: Context?
-        return new DidDoc([],
-            document.Id,
-            document.AlsoKnownAs?.ToList() ?? [],
-            document.VerificationMethod?.Select(method => method.ToVerificationMethod()).ToList() ?? [],
-            document.Service?.Select(service => service.ToService()).ToList() ?? []);
+        public DidDoc ToDidDoc()
+        {
+            // TODO: Context?
+            return new DidDoc([],
+                document.Id,
+                document.AlsoKnownAs?.ToList() ?? [],
+                document.VerificationMethod?.Select(method => method.ToVerificationMethod()).ToList() ?? [],
+                document.Service?.Select(service => service.ToService()).ToList() ?? []);
+        }   
     }
 
-    public static VerificationMethod ToVerificationMethod(this CommonWeb.VerificationMethod method)
+    extension(CommonWeb.VerificationMethod method)
     {
-        return new VerificationMethod(method.Id, method.Type, method.Controller, method.PublicKeyMultibase!);
-    }
+        public VerificationMethod ToVerificationMethod()
+        {
+            return new VerificationMethod(method.Id, method.Type, method.Controller, method.PublicKeyMultibase!);
+        }
+    }   
 
-    public static Service ToService(this CommonWeb.Service method)
+    extension(CommonWeb.Service method)
     {
-        return new Service(method.Id, method.Type, method.ServiceEndpoint);
+        public  Service ToService()
+        {
+            return new Service(method.Id, method.Type, method.ServiceEndpoint);
+        }
     }
 }
